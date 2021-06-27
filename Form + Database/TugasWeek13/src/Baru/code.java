33aa0d93327cbb4ea528238952d2f4ffd054cc8b
Alvin Pradana Antony <quickgaming321@gmail.com>
2021-06-27 13:07:19 +0700

    Form + Database

---
+++ Form + Database/TugasWeek13/src/Baru/code.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Baru;
+
+/**
+ *
+ * @author Tony's PC
+ */
+public class code {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        log A = new log();
+        A.setVisible(true);
+    }
+    
+}
